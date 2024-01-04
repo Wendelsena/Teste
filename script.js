76fb9564094f2532ffb08a0b4be5de1bd435873d
Wendelsena <wendel4019@gmail.com>
2024-01-03 23:26:30 -0300

    Site Base Teste

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('botaoTrocaTexto').addEventListener ('click', function() {
+ var paragrafo = document.getElementById('paragrafoTexto');
+ paragrafo.textContent = 'Texto Alterado! Clique novamente para reverter.';
+});
